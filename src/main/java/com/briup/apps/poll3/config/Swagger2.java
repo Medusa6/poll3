6348b79f46cdec1a2d7e8264772cef5a024e53ba
Administrator <Administrator@USER-20170930NW>
2018-06-25 18:32:52 +0800

    第二次

--- src/main/java/com/briup/apps/poll3/config/Swagger2.java
+++ src/main/java/com/briup/apps/poll3/config/Swagger2.java
@@ -38,7 +38,7 @@ public class Swagger2 {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.briup.apps.poll.web"))
+				.apis(RequestHandlerSelectors.basePackage("com.briup.apps.poll3.web"))
 				.paths(PathSelectors.any())
 				.build();
 	}
